4ba189561054a040cfe77fee4d56a0a4d63a3442
StayAbroad <dennis.koester11@gmail.com>
2023-03-07 22:59:54 +0100

    firestore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'simple-crm-317dd',
+    appId: '1:261739967723:web:eaa0b0c4ff212285247aae',
+    storageBucket: 'simple-crm-317dd.appspot.com',
+    apiKey: 'AIzaSyCA681DjyRtsUC9O4MiI7C7lmClK4s6Bmg',
+    authDomain: 'simple-crm-317dd.firebaseapp.com',
+    messagingSenderId: '261739967723',
+  },};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCA681DjyRtsUC9O4MiI7C7lmClK4s6Bmg',
+    authDomain: 'simple-crm-317dd.firebaseapp.com',
+    projectId: 'simple-crm-317dd',
+    storageBucket: 'simple-crm-317dd.appspot.com',
+    messagingSenderId: '261739967723',
+    appId: '1:261739967723:web:eaa0b0c4ff212285247aae',
+  },
+};
